e8477029cc3e0c1daf7eb142a7eb6b50e3c3d124
the Black Cat (ASAD) <kitkitcat.welcome@gmail.com>
2022-11-11 19:32:54 +0600

    Add testimolial

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,8 +1,69 @@
 import React from 'react'
 import './testimonial.css'
+import clientImage1 from '../../assets/avatar1.jpg'
+import clientImage2 from '../../assets/avatar2.jpg'
+import clientImage3 from '../../assets/avatar3.jpg'
+import clientImage4 from '../../assets/avatar4.jpg'
+
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 function Testimonial() {
   return (
-    <section id='testimonial'>Testimonial</section>
+    <section id='testimonial'>
+      <h5>Review from clients and teammates</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}
+      className="mySwiper container testimonials__container">
+        <SwiperSlide className="testimonial">
+          <div className="client__avatar">
+            <img src={clientImage4} alt="Client Photo" />
+          </div>
+          <h5 className='client__name'>Arial Cums</h5>
+          <small className='client__review' >
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta hic quis dolores earum perspiciatis sunt reiciendis! Quis labore nostrum dolorem.
+          </small>
+        </SwiperSlide>
+        <SwiperSlide className="testimonial">
+          <div className="client__avatar">
+            <img src={clientImage2} alt="Client Photo" />
+          </div>
+            <h5 className='client__name'>Cosmel Villae</h5>
+            <small className='client__review' >
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta hic quis dolores earum perspiciatis sunt reiciendis! Quis labore nostrum dolorem.
+            </small>
+        </SwiperSlide>
+
+        <SwiperSlide className="testimonial">
+          <div className="client__avatar">
+            <img src={clientImage3} alt="Client Photo" />
+          </div>
+            <h5 className='client__name'>Gubrial Swarez</h5>
+            <small className='client__review' >
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta hic quis dolores earum perspiciatis sunt reiciendis! Quis labore nostrum dolorem.
+            </small>
+        </SwiperSlide>
+
+        <SwiperSlide className="testimonial">
+          <div className="client__avatar">
+            <img src={clientImage1} alt="Client Photo" />
+          </div>
+            <h5 className='client__name'>Recardo Mountino</h5>
+            <small className='client__review' >
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta hic quis dolores earum perspiciatis sunt reiciendis! Quis labore nostrum dolorem.
+            </small>
+        </SwiperSlide>
+      </Swiper>
+    </section>
   )
 }
 
